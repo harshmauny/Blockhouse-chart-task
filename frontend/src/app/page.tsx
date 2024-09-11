5efb2379358f0e08df7811976fa99fb3463cc457
harshmauny <harsh.mauny@gmail.com>
2024-09-11 17:49:40 -0400

    Loader added in frontend to fix the fetch error issue

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,6 +8,7 @@ import {
   CandleStickChartComponent,
 } from "./components/charts";
 import Header from "./components/header";
+import { Spinner } from "@material-tailwind/react";
 
 export default function Dashboard() {
   const [chartData, setChartData] = useState({
@@ -16,10 +17,13 @@ export default function Dashboard() {
     bar: { labels: [], data: [] },
     pie: { labels: [], data: [] },
   });
+  const [fetchError, setFetchError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const [candlestick, line, bar, pie] = await Promise.all([
           axios.get("http://localhost:8000/api/candlestick-data/"),
           axios.get("http://localhost:8000/api/line-chart-data/"),
@@ -33,23 +37,46 @@ export default function Dashboard() {
           bar: bar.data,
           pie: pie.data,
         });
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setFetchError(true);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
-  return (
-    <div className="">
-      <Header />
-      <div className="max-w-[1200px]  mx-auto grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
-        <CandleStickChartComponent data={chartData.candlestick} />
-        <LineChartComponent data={chartData.line} />
-        <BarChartComponent data={chartData.bar} />
-        <PieChartComponent data={chartData.pie} />
+  if (loading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <Spinner
+          className="h-12 w-12"
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        />
       </div>
+    );
+  }
+
+  return (
+    <div className="dashboard">
+      {fetchError ? (
+        <div className="h-screen flex justify-center items-center">
+          <div className="font-semibold text-xl ">Not Found</div>
+        </div>
+      ) : (
+        <>
+          <Header />
+          <div className="max-w-[1200px]  mx-auto grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+            <CandleStickChartComponent data={chartData.candlestick} />
+            <LineChartComponent data={chartData.line} />
+            <BarChartComponent data={chartData.bar} />
+            <PieChartComponent data={chartData.pie} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
